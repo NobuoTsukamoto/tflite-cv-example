07b9a065897c3118b35e4f36e259a616c0fc66ea
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2019-07-31 20:51:44 +0900

    formmater

--- camouflage/camouflage_picamera.py
+++ camouflage/camouflage_picamera.py
@@ -25,11 +25,11 @@ import PIL
 
 from utils import visualization as visual
 
-WINDOW_NAME = 'Inpaint Pi Camera'
+WINDOW_NAME = "Inpaint Pi Camera"
 
 # Function to read labels from text files.
 def ReadLabelFile(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         lines = f.readlines()
 
     ret = {}
@@ -38,24 +38,23 @@ def ReadLabelFile(file_path):
         ret[int(pair[0])] = pair[1].strip()
     return ret
 
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="File path of Tflite model.", required=True)
+    parser.add_argument("--label", help="File path of label file.", required=True)
+    parser.add_argument("--top_k", help="keep top k candidates.", default=3)
     parser.add_argument(
-            '--model', help='File path of Tflite model.', required=True)
-    parser.add_argument(
-            '--label', help='File path of label file.', required=True)
-    parser.add_argument(
-            '--top_k', help="keep top k candidates.", default=3)
-    parser.add_argument(
-            '--threshold', help="threshold to filter results.", type=float, default=0.5)
-    parser.add_argument(
-            '--width', help="Resolution width.", default=640)
-    parser.add_argument(
-            '--height', help="Resolution height.", default=480)
+        "--threshold", help="threshold to filter results.", type=float, default=0.5
+    )
+    parser.add_argument("--width", help="Resolution width.", default=640)
+    parser.add_argument("--height", help="Resolution height.", default=480)
     args = parser.parse_args()
 
     # Initialize window.
-    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_AUTOSIZE | cv2.WINDOW_KEEPRATIO)
+    cv2.namedWindow(
+        WINDOW_NAME, cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_AUTOSIZE | cv2.WINDOW_KEEPRATIO
+    )
     cv2.moveWindow(WINDOW_NAME, 100, 200)
 
     # Initialize engine.
@@ -80,9 +79,9 @@ def main():
         time.sleep(0.1)
 
         try:
-            for frame in camera.capture_continuous(rawCapture,
-                                                 format='rgb',
-                                                 use_video_port=True):
+            for frame in camera.capture_continuous(
+                rawCapture, format="rgb", use_video_port=True
+            ):
                 start_ms = time.time()
 
                 rawCapture.truncate(0)
@@ -92,8 +91,13 @@ def main():
                 input_buf = PIL.Image.fromarray(image)
 
                 # Run inference.
-                ans = engine.DetectWithImage(input_buf, threshold=args.threshold,
-                       keep_aspect_ratio=False, relative_coord=False, top_k=args.top_k)
+                ans = engine.DetectWithImage(
+                    input_buf,
+                    threshold=args.threshold,
+                    keep_aspect_ratio=False,
+                    relative_coord=False,
+                    top_k=args.top_k,
+                )
 
                 # Display result.
                 if is_inpaint_mode == True:
@@ -103,17 +107,19 @@ def main():
                             if labels and obj.label_id in labels:
                                 # Draw a mask rectangle.
                                 box = obj.bounding_box.flatten().tolist()
-                                visual.draw_rectangle(mask, box, (255, 255, 255), thickness=-1)
+                                visual.draw_rectangle(
+                                    mask, box, (255, 255, 255), thickness=-1
+                                )
 
                     # Image Inpainting
-                    dst = cv2.inpaint(im, mask,3,cv2.INPAINT_TELEA)
+                    dst = cv2.inpaint(im, mask, 3, cv2.INPAINT_TELEA)
                     # dst = cv2.inpaint(im, mask,3,cv2.INPAINT_NS)
-                    
+
                 else:
                     for obj in ans:
                         if labels and obj.label_id in labels:
                             label_name = labels[obj.label_id]
-                            caption = '{0}({1:.2f})'.format(label_name, obj.score)
+                            caption = "{0}({1:.2f})".format(label_name, obj.score)
 
                             # Draw a rectangle and caption.
                             box = obj.bounding_box.flatten().tolist()
@@ -126,18 +132,17 @@ def main():
                 fps = 1 / elapsed_ms
 
                 # Display fps
-                fps_text = '{0:.2f}ms, {1:.2f}fps'.format(
-                        (elapsed_ms * 1000.0), fps)
+                fps_text = "{0:.2f}ms, {1:.2f}fps".format((elapsed_ms * 1000.0), fps)
                 visual.draw_caption(dst, (10, 30), fps_text)
 
                 # Display image
                 cv2.imshow(WINDOW_NAME, dst)
                 key = cv2.waitKey(10) & 0xFF
-                if key == ord('q'):
+                if key == ord("q"):
                     break
-                elif key == ord(' '):
+                elif key == ord(" "):
                     is_inpaint_mode = not is_inpaint_mode
-                    print('inpant mode change :', is_inpaint_mode)
+                    print("inpant mode change :", is_inpaint_mode)
 
         finally:
             camera.stop_preview()
@@ -145,5 +150,6 @@ def main():
     # When everything done, release the window
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- classify/classify_capture_picamera.py
+++ classify/classify_capture_picamera.py
@@ -24,23 +24,19 @@ import PIL
 
 from utils import visualization as visual
 
-WINDOW_NAME = 'Edge TPU Image classification'
+WINDOW_NAME = "Edge TPU Image classification"
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-            '--model', help='File path of Tflite model.', required=True)
-    parser.add_argument(
-            '--label', help='File path of label file.', required=True)
-    parser.add_argument(
-            '--top_k', help="keep top k candidates.", default=3, type=int)
-    parser.add_argument(
-            '--width', help="Resolution width.", default=640, type=int)
-    parser.add_argument(
-            '--height', help="Resolution height.", default=480, type=int)
+    parser.add_argument("--model", help="File path of Tflite model.", required=True)
+    parser.add_argument("--label", help="File path of label file.", required=True)
+    parser.add_argument("--top_k", help="keep top k candidates.", default=3, type=int)
+    parser.add_argument("--width", help="Resolution width.", default=640, type=int)
+    parser.add_argument("--height", help="Resolution height.", default=480, type=int)
     args = parser.parse_args()
 
-    with open(args.label, 'r') as f:
+    with open(args.label, "r") as f:
         pairs = (l.strip().split(maxsplit=1) for l in f.readlines())
         labels = dict((int(k), v) for k, v in pairs)
 
@@ -64,9 +60,9 @@ def main():
         # allow the camera to warmup
         time.sleep(0.1)
         try:
-            for frame in camera.capture_continuous(rawCapture,
-                                                 format='rgb',
-                                                 use_video_port=True):
+            for frame in camera.capture_continuous(
+                rawCapture, format="rgb", use_video_port=True
+            ):
                 rawCapture.truncate(0)
 
                 image = frame.array
@@ -80,34 +76,36 @@ def main():
                 # Check result.
                 if results:
                     for i in range(len(results)):
-                        label = '{0} ({1:.2f})'.format(
-                            labels[results[i][0]], results[i][1])
+                        label = "{0} ({1:.2f})".format(
+                            labels[results[i][0]], results[i][1]
+                        )
                         pos = 60 + (i * 30)
                         visual.draw_caption(im, (10, pos), label)
 
                 # Calc fps.
                 fps = 1 / elapsed_ms
                 elapsed_list.append(elapsed_ms)
-                avg_text = ''
+                avg_text = ""
                 if len(elapsed_list) > 100:
                     elapsed_list.pop(0)
                     avg_elapsed_ms = np.mean(elapsed_list)
                     avg_fps = 1 / avg_elapsed_ms
-                    avg_text = ' AGV: {0:.2f}ms, {1:.2f}fps'.format(
-                        (avg_elapsed_ms * 1000.0), avg_fps)
+                    avg_text = " AGV: {0:.2f}ms, {1:.2f}fps".format(
+                        (avg_elapsed_ms * 1000.0), avg_fps
+                    )
 
                 # Display fps
-                fps_text = '{0:.2f}ms, {1:.2f}fps'.format(
-                        (elapsed_ms * 1000.0), fps)
+                fps_text = "{0:.2f}ms, {1:.2f}fps".format((elapsed_ms * 1000.0), fps)
                 visual.draw_caption(im, (10, 30), fps_text + avg_text)
 
-                 # display
+                # display
                 cv2.imshow(WINDOW_NAME, im)
-                if cv2.waitKey(10) & 0xFF == ord('q'):
+                if cv2.waitKey(10) & 0xFF == ord("q"):
                     break
 
         finally:
             camera.stop_preview()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
